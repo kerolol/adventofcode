0da195322ad62d40379a48dc806291879cd715c8
Patrick Manser <patrick.manser@software-brauerei.ch>
2020-12-10 16:58:52 +0100

    clean up

--- day3/part1.ts
+++ day3/part1.ts
@@ -4,24 +4,17 @@ const countTrees = (input: string): number => {
     let pos = 0;
     let count = 0;
 
-    input.split('\n').forEach((line: string, idx: number) => {
-        // Continue if first iteration
+    input.split('\n').forEach((line: string, idx: number): void => {
         if (idx === 0) {
             return;
         }
 
-        // Walk the steps
         pos += 3;
 
-        // Check if position is at the end of the line
         if (pos >= line.length) {
             pos -= line.length;
         }
 
-        console.log('I am on line ' + idx + ' at position ' + pos + ' and here is a ' + line[pos]);
-
-        // Check if tree is located at current position
-        // If yes, increase counter
         if (line[pos] === '#') {
             count++;
         }
